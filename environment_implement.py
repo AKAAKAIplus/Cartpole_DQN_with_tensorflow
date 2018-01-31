6bbe9b30da863d18386762b875e2a31b9aa821fd
Kai <twnoken@gmail.com>
2018-01-31 16:22:36 +0900

    update environment and batch

--- environment_implement.py
+++ environment_implement.py
@@ -13,7 +13,7 @@ observation = env.reset()
 DQN = double_network_DQN(num_of_actions=env.action_space.n, num_of_features=env.observation_space.shape[0])
 
 count = 0
-for num_of_episode in range(3000):
+for num_of_episode in range(1000):
 
     observation = env.reset()
     ep_r = 0
@@ -31,15 +31,16 @@ for num_of_episode in range(3000):
 
 
         x, x_dot, theta, theta_dot = observation_new
+
         r1 = (env.x_threshold - abs(x))/env.x_threshold - 0.8
-        r2 = (env.theta_threshold_radians - abs(theta))/env.theta_threshold_radians - 0.5
-        reward = r1 + r2*1.2
+        r2 = (env.theta_threshold_radians - abs(theta))/env.theta_threshold_radians
+        reward = r1 + r2
 
         DQN.learning_buffers(observation, action, reward, observation_new)
 
-        if  count > 1000:
-        	DQN.learn()
-
+        if  count > 100:
+            DQN.learn()
+            # print('learning')
         if done:
             end = time.time()
             print("Time taken: "+str(end - start)+"s")
